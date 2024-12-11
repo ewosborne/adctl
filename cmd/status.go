612ef0bc9f36987e5591aa49920e845c4484e9c6
Eric Osborne <erico@netflix.com>
2024-12-11 13:30:40 -0500

    restructured cli

--- cmd/status.go
+++ cmd/status.go
@@ -1,94 +1,28 @@
 /*
-Copyright © 2024 Eric Osborne
-No header.
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-	"time"
-
-	"github.com/ewosborne/adctl/common"
 	"github.com/spf13/cobra"
 )
-
-type Status struct {
-	Protection_enabled           bool
-	Protection_disabled_duration uint64
-}
 
 // statusCmd represents the status command
 var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "Get adblock status",
-	RunE:  StatusCmdE,
-}
-
-func StatusCmdE(cmd *cobra.Command, args []string) error {
-	s, err := GetStatus()
-	if err != nil {
-		return err
-	}
-	return PrintStatus(s)
-}
-
-// statusCommand prints something intelligent about what's in status
-func PrintStatus(status Status) error {
-	// status, err := GetStatus()
-	// if err != nil {
-	// 	return fmt.Errorf("error getting status: %w", err)
-	// }
-
-	var statusString string
-	if status.Protection_enabled {
-		statusString = "enabled"
-	} else {
-		statusString = "disabled"
-	}
-
-	if status.Protection_disabled_duration > 0 {
-		remaining := time.Duration(
-			status.Protection_disabled_duration * uint64(time.Millisecond)).Truncate(time.Second)
-
-		statusString += fmt.Sprintf(" for %v", remaining)
-	}
-
-	fmt.Printf("adguard is %v\n", statusString)
-
-	return nil
-}
-
-func GetStatus() (Status, error) {
-	var ret Status
-	// get status
-	// then return it?
-
-	// build the command, it's specific to status
-	baseURL, err := common.GetBaseURL()
-	if err != nil {
-		return ret, err
-	}
-	baseURL.Path = "/control/status"
-
-	statusQuery := common.CommandArgs{
-		Method: "GET",
-		URL:    baseURL,
-	}
-
-	body, err := common.SendCommand(statusQuery)
-	if err != nil {
-		return ret, err
-	}
-
-	// serialize body into Status and return appropriately
-	var s Status
-	json.Unmarshal(body, &s)
-
-	return s, nil
+	Short: "Check and change adblocking status",
 }
 
 func init() {
 	rootCmd.AddCommand(statusCmd)
 
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
