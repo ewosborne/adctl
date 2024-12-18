43952620bb931cb2f3919830ddc53e83ac357d2e
Eric Osborne <erico@netflix.com>
2024-12-18 10:16:59 -0500

    test changes

--- cmd/rewrite_test.go
+++ cmd/rewrite_test.go
@@ -6,10 +6,22 @@ import (
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
-func TestRewrite_AddListDeleteCLI(t *testing.T) {
+func TestRewrite_AddListCLI(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Setup: setupEnv,
-		Files: []string{"testdata/script/rewrite_list.txtar"},
+		Files: []string{
+			"testdata/script/rewrite_add_list.txtar",
+		},
+	},
+	)
+}
+
+func TestRewrite_DeleteCLI(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Setup: setupEnv,
+		Files: []string{
+			"testdata/script/rewrite_delete.txtar",
+		},
 	},
 	)
 }
